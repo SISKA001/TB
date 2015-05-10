958ab751d926e7c0d2aa9aed043656d83532f804
SISKA001 <ayusiska.yuana001@gmail.com>
2015-05-10 16:21:20 +0700

    PBO

--- src/tb/spesivikasi_kebersihan_ruangan.java
+++ src/tb/spesivikasi_kebersihan_ruangan.java
@@ -13,7 +13,7 @@ import java.util.Scanner;
  *
  * @author Asus
  */
-public class spesivikasi_kebersihan_ruangan {
+public abstract class spesivikasi_kebersihan_ruangan {
      private String kondisi_lantai;
     private String kondisi_atap;
     private String kondisi_pintu;
@@ -24,73 +24,89 @@ public class spesivikasi_kebersihan_ruangan {
     
 public void setkondisi_lantai(String kondisi_lantai){
         this.kondisi_lantai = kondisi_lantai;
-        String KL = scan.nextLine();
-        if("Baik".equals(KL))
-            System.out.println("Sesuai kriteria");
-        else if("Buruk".equals(KL))
-            System.out.println("Tidak Sesuai Kriteria");
-        else
-            System.out.println("Inputan Salah");
-}
+       }
     public String getkondisi_lantai(){
             return this.kondisi_lantai;
-           
-        }
+            }
       public void setkondisi_atap (String kondisi_atap){
         this.kondisi_atap = kondisi_atap;
-        String KAP = scan.nextLine();
-        if("Baik".equals(KAP))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if("Buruk".equals(KAP))
-              System.out.println("Kondisi Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan Salah");
-        
-    }
+        }
      public String getkondisi_atap(){
             return this.kondisi_atap;
         }
       public void setkondisi_pintu (String kondisi_pintu){
         this.kondisi_pintu = kondisi_pintu;
-        String KP = scan.nextLine();
-        if("Baik".equals(KP))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if ("Buruk".equals(KP))
-              System.out.println("Kondisi Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan Salah");
-    }
+        }
      public String getkondisi_pintu(){
             return this.kondisi_pintu;
         }
       public void setkondisi_cendela (String kondisi_cendela){
         this.kondisi_cendela = kondisi_cendela;
-        String KC = scan.nextLine();
-        if("Baik".equals(KC))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if("Burk".equals(KC))
-              System.out.println("Kondisi Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan Salah");
-    }
+        }
      public String getkondisi_cendela(){
             return this.kondisi_cendela;
         }
       public void setkondisi_dinding (String kondisi_dinding){
         this.kondisi_dinding = kondisi_dinding;
-        String KDG = scan.nextLine();
-        if("Baik".equals(KDG))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if("Buruk".equals(KDG))
-              System.out.println("Kondisi Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan Salah");
-    }
+        }
      public String getkondisi_dinding(){
             return this.kondisi_dinding;
         }
-
-    void setkondisi_jendela(String nextLine) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+String kondisi_lantai (String kondisi_lantai){
+    if("Baik".equals(kondisi_lantai)){
+        return "Sesuai";
+    }
+    else if("Buruk".equals(kondisi_lantai)){
+        return "Tidak Sesuai";
+    }
+    else{
+        return "Inputan Salah";
+    }    
+    }
+String kondisi_atap (String kondsi_atap){
+    if("Baik".equals(kondisi_atap)){
+        return "Sesuai";
+}
+    else if("Buruk".equals(kondisi_atap)){
+        return "Tidak Sesuai";
+    }
+    else{
+        return "Inputan Salah";
     }
 }
+String kondisi_pintu (String kondisi_pintu){
+    if("Baik".equals(kondisi_pintu)){
+        return "Sesuai";
+    }
+    else if("Buruk".equals(kondisi_pintu)){
+        return "Tidak Sesuai";
+    }
+    else{
+        return "Inputan Salah";
+    }
+}
+String kondisi_cendela (String kondisi_cendela){
+    if("Baik".equals(kondisi_cendela)){
+        return "Sesuai";
+    }
+    else if("Buruk".equals(kondisi_cendela)){
+        return "Tidak Sesuai";
+    }
+    else{
+        return "Inputan Salah";
+    }
+}
+String kodisi_dinding (String kondisi_dinding){
+    if("Baik".equals(kondisi_dinding)){
+        return "Sesuai";
+    }
+    else if("Buruk".equals(kondisi_dinding)){
+        return "Tidak Sesuai";
+    }
+    else{
+        return "Inputan Salah";
+    }
+}    
+abstract void analisis();
+}
+ 
