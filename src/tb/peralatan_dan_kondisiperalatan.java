a01d4a63d589859a131cb39697078673f8b5238a
SISKA001 <ayusiska.yuana001@gmail.com>
2015-05-10 16:21:13 +0700

    PBO

--- src/tb/peralatan_dan_kondisiperalatan.java
+++ src/tb/peralatan_dan_kondisiperalatan.java
@@ -13,10 +13,9 @@ import java.util.Scanner;
  *
  * @author Asus
  */
-public class peralatan_dan_kondisiperalatan {
+public abstract class peralatan_dan_kondisiperalatan {
    private int jumlah_lampu;
     private String kondisi_lampu;
-    private String warna_lampu;
     private String posisi_lampu;
     private int jumlah_kipas_angin;
     private String kondisi_kipas_angin;
@@ -30,7 +29,6 @@ public class peralatan_dan_kondisiperalatan {
     private String kondisi_stopkontak;
     private String posisi_stopkontak;
     private String pilihanSSID;
-    private String bandwinth;
     private int jumlah_CCTV;
     private String kondisi_CCTV;
     private String posisi_CCTV;
@@ -39,186 +37,86 @@ public class peralatan_dan_kondisiperalatan {
 
      public void setjumlah_lampu(int jumlah_lampu){
         this.jumlah_lampu = jumlah_lampu;
-        if(jumlah_lampu >=6){
-            System.out.println("sesuai");
-        }
-        else if(jumlah_lampu <6){
-            System.out.println("tidak sesuai");
-        }
-    }
+       }
      public int getjumlah_lampu(){
             return this.jumlah_lampu;
         }
       public void setkondisi_lampu (String kondisi_lampu){
         this.kondisi_lampu = kondisi_lampu;
-      String KL = scan.nextLine();
-      if ("Baik".equals(KL))
-              System.out.println("Kondisi sesuai kriteria");
-      else if("Buruk".equals(KL))
-              System.out.println("Kondisi tidak sesuai kriteria");
-      else 
-              System.out.println("inputan salah");
          }
      public String getkondisi_lampu(){
             return this.kondisi_lampu;   
-        }
-      public void setwarna_lampu (String warna_lampu){
-        this.warna_lampu = warna_lampu;
-    }
-     public String getwarna_lampu(){
-            return this.warna_lampu;
-        }
+     }
       public void setposisi_lampu (String posisi_lampu){
         this.posisi_lampu = posisi_lampu;
-        String PL = scan.nextLine();
-        if("Kiri Depan, Kiri Tengah, Kiri Belakang".equals(PL))
-              System.out.println("Sesuai Kriteria");
-        else if("Kanan Depan, Kanan Tengah, Kanan Belakang".equals(PL))
-              System.out.println("Sesuai Kriteria");
-        else 
-              System.out.println("Inputan salah");  }
-    
+      }
       public String getposisi_lampu(){
             return this.posisi_lampu;
         }
       public void setjumlah_kipas_angin (int jumlah_kipas_angin){
         this.jumlah_kipas_angin= jumlah_kipas_angin;
-        if (jumlah_kipas_angin >=2){
-            System.out.println("sesuai");
-        }
-        else if(jumlah_kipas_angin <2){
-            System.out.println("tidak sesuai");
-        }
-    }
+       }
      public int getjumlah_kipas_angin(){
             return this.jumlah_kipas_angin;
         }
       public void setkondisi_kipas_angin (String posisi_kipas_angin){
         this.posisi_kipas_angin= posisi_kipas_angin;
-        String KA = scan.nextLine();
-        if("Baik".equals(KA))
-              System.out.println("Sesuai Kriteria");
-        else if("Buruk".equals(KA))
-              System.out.println("Tidak Sesuai Kriteria");
-        else
-              System.out.println("inputan salah");
-    }
+       }
      public String getkondisi_kipas_angin(){
             return this.kondisi_kipas_angin;
         }
       public void setposisi_kipas_angin (String posisi_kipas_angin){
         this.posisi_kipas_angin = posisi_kipas_angin;
-        String PKA = scan.nextLine();
-        if ("Kiri Depan, Kanan tengah".equals(PKA))
-              System.out.println("Sesuai Kriteria");
-        else if("Belakang".equals(PKA))
-              System.out.println("Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan salah");
-    }
+       }
      public String getposisi_kipas_angin(){
             return this.posisi_kipas_angin;
         }
       public void setjumlah_AC (int jumlah_AC){
         this.jumlah_AC = jumlah_AC;
-        if (jumlah_AC >=2){
-            System.out.println("sesuai");
-        }
-        else if(jumlah_AC <2){
-            System.out.println("tidak sesuai");
-        }
-    }
+       }
      public int getjumlah_AC(){
             return this.jumlah_AC;
         }
       public void setkondisi_AC (String kondisi_AC){
         this.kondisi_AC = kondisi_AC;
-        String KAC = scan.nextLine();
-        if("Baik".equals(KAC))
-              System.out.println("Sesuai Kriteria");
-        else if("Buruk".equals(KAC))
-              System.out.println("Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan salah");
-    }
+        }
      public String getkondsi_AC(){
             return this.kondisi_AC;
         }
       public void setposisiAC (String posisiAC){
         this.posisiAC = posisiAC;
-        String PAC = scan.nextLine();
-        if("Pojok Depan".equals(PAC))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if("Pojok Belakang".equals(PAC))
-              System.out.println("Kondisi Tidak Sesuai");
-        else 
-              System.out.println("Inputan Salah");
-    }
+       }
      public String getposisiAC(){
             return this.posisiAC;
         }
       public void setjumlah_LCD (int jumlah_LCD){
         this.jumlah_LCD = jumlah_LCD;
-        if (jumlah_LCD >=1){
-                System.out.println("sesuai");
-            }
-            else if(jumlah_LCD <1){
-                System.out.println("tidak ");
-            }
-    }
+       }
      public int getjumlah_LCD(){
             return this.jumlah_LCD;
             
         }
       public void setkodisi_LCD(String kondisi_LCD){
         this.kondisi_LCD = kondisi_LCD;
-        String KLCD = scan.nextLine();
-        if ("Baik".equals(KLCD))
-              System.out.println("Sesuai Kriteria");
-        else if ("Buruk".equals(KLCD))
-              System.out.println("Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan Salah");
-    }
+       }
      public String getkondisi_LCD(){
             return this.kondisi_LCD;
         }
       public void setjumlah_stopkontak (int jumlah_stopkontak){
         this.jumlah_stopkontak = jumlah_stopkontak;
-        if (jumlah_stopkontak >=6){
-            System.out.println("sesuai");
-        }
-        else if(jumlah_stopkontak <6){
-            System.out.println("tidak sesuai");
-        }
-    }
+       }
      public int getjumlah_stopkontak(){
             return this.jumlah_stopkontak;
         }
      public void setkondisi_stopkontak (String kondisi_stopkontak){
          this.kondisi_stopkontak = kondisi_stopkontak;
-         String KSK = scan.nextLine();
-         if("Baik".equals(KSK))
-             System.out.println("Sesuai Kriteria");
-         else if("Buruk".equals(KSK))
-             System.out.println("Tidak Sesuai Kriteria");
-         else
-             System.out.println("Inputan Salah");
-     }
+       }
      public String getkondisi_stopkontak(){
          return this.kondisi_stopkontak;
      }
       public void setposisi_stopkontak (String posisi_stopkontak){
         this.posisi_stopkontak = posisi_stopkontak;
-        String PST = scan.nextLine();
-        if("Kanan Depan, Kanan Kiri".equals(PST))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if("Kanan Belakang, Kiri Belakang".equals(PST))
-              System.out.println("Kondisi idak Sesuai Kriteria");
-        else
-              System.out.println("inputan salah");
-            
-    }
+       }
      public String getposisi_stopkontak(){
             return this.posisi_stopkontak;
         }
@@ -228,62 +126,153 @@ public class peralatan_dan_kondisiperalatan {
      public String getpilihanSSID(){
             return this.pilihanSSID;
         }
-      public void setbandwinth (String bandwinth){
-        this.bandwinth = bandwinth;
-    }
-     public String getbandwinth(){
-            return this.bandwinth;
-        }
       public void setjumlah_CCTV (int jumlah_CCTV){
         this.jumlah_CCTV = jumlah_CCTV;
-        if (jumlah_CCTV >=1){
-            System.out.println("sesuai");
-        }
-        else if(jumlah_CCTV <1){
-            System.out.println(" tidak sesuai");
-        }
-    }
+       }
      public int getjumlsh_CCTV(){
             return this.jumlah_CCTV;
         }
       public void setkondisi_CCTV (String kondisi_CCTV){
         this.kondisi_CCTV = kondisi_CCTV;
-        String KCCTV = scan.nextLine();
-        if("Baik".equals(KCCTV))
-              System.out.println("Sesuai Kriteria");
-        else if ("Buruk".equals(KCCTV))
-              System.out.println("Tidak Sesuai Kriteria");
-        else
-              System.out.println("Inputan Salah");
-    }
+       }
      public String getkondisi_CCTV(){
             return this.kondisi_CCTV;
         }
       public void setposisi_CCTV (String posisi_CCTV){
         this.posisi_CCTV = posisi_CCTV;
-        String PCCTV = scan.nextLine();
-        if("Pojok Depan".equals(PCCTV))
-              System.out.println("Posisi Sesuai Kriteria");
-        else if("Pojok Belakang".equals(PCCTV))
-              System.out.println("Posisi Tidak Sesuai Kriteria");
-        else
-              System.out.println("inputan salah");
-    }
+       }
      public String getposisi_CCTV(){
             return this.posisi_CCTV;
-        }
-
-    void setkondisi_LCD(String nextLine) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void setposisi_LCD(String nextLine) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    void setbandwidth(String nextLine) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-     
+     } 
+     String lampu (int jumlah_lampu,String kondisi_lampu,String posisi_lampu){
+         if(jumlah_lampu >=4){
+             return "Sesuai";
+         }
+         else if(jumlah_lampu <4){
+             return "Tidak Sesuai";
+         }
+         else if ("Kanan depan kanan belakang dan Kiri depan kiri belakang".equals(posisi_lampu)){
+            return "Sesuai";  
+         }
+            else if("Baik".equals(kondisi_lampu)){
+                    return "Sesuai";
+                    }
+            else if("Buruk".equals(kondisi_lampu)){
+                return "Tidak Sesuai";
+            }
+         else{
+             return "Inputan Salah";
+         }
+     }
+     String kipas_angin (int jumlah_kipas_angin, String kondisi_kipas_angin, String posisi_kipas_angin){
+         if(jumlah_kipas_angin >=2){
+             return "Sesuai";
+         }
+         else if(jumlah_kipas_angin <2){
+             return "Tidak Sesuai";
+         }
+         
+         else if("Kanan Tengah".equals(posisi_kipas_angin) ||"Kiri Tengah".equals(posisi_kipas_angin)){
+             return "Sesuai";
+         }
+         else if("Baik".equals(kondisi_kipas_angin)){
+             return "Sesuai";
+         }
+         else if("Buruk".equals(kondisi_kipas_angin)){
+             return "Tidak Sesuai";
+         }
+         else{
+             return "Inputan salah";
+         }    
+     }
+     String AC (int jumlah_AC, String kondisi_AC, String posisi_AC){
+         if(jumlah_AC >=1){
+             return "Sesuai";
+         }
+         else if(jumlah_AC <1){
+             return "Tidak Sesuai";
+         }
+         else if("Kanan Depan".equals(posisi_AC)){
+             return "Sesuai";
+         }
+         else if("Baik".equals(kondisi_AC)){
+             return "Sesuai";
+         }
+         else if("Buruk".equals(kondisi_AC)){
+             return "Tidak Sesuai";
+         }
+         else{
+             return "Inputan Salah";
+         }
+     }
+     String LCD (int jumlah_LCD, String kondisi_LCD, String posisi_LCD){
+         if(jumlah_LCD >=1){
+             return "Sesuai";
+         }
+         else if(jumlah_LCD >1){
+             return "Tidak Sesuai";
+         }
+         else if("Baik".equals(kondisi_LCD)){
+             return "Seseuai";
+         }
+         else if("Buruk".equals(kondisi_LCD)){
+             return "Tidak Sesuai";
+         }
+         else if("Tengah Depan".equals(posisi_LCD)){
+             return "Sesuai";
+         }
+         else{
+             return "Inputan Salah";
+         }
+     }
+     String stopkontak (int jumlah_stopkontak, String kondisi_stopkontak, String posisi_stopkontak){
+         if(jumlah_stopkontak >=2){
+             return "Sesuai";
+         }
+         else if(jumlah_stopkontak <2){
+             return "Tidak Sesuai";
+         }
+         else if("Kanan Depan dan Kiri Depan".equals(posisi_stopkontak)){
+             return "Sesuai";
+         }
+         else if("Baik".equals(kondisi_stopkontak)){
+             return "Sesuai";
+         }
+         else if("Buruk".equals(kondisi_stopkontak)){
+             return "Tidak Sesuai";
+         }
+         else{
+             return "Inputan salah";
+         }
+     }
+     String SSID (String pilihanSSID){
+         if("UMM Hotspot".equals(pilihanSSID)){
+             return "Sesuai";
+         }
+         else if("Lab IT".equals(pilihanSSID)){
+             return "Sesuai";
+         }
+         else{
+             return "Inputan Salah";
+         }
+     }
+     String CCV (int jumlah_CCTV,String kondisi_CCTV, String posisi_CCTV){
+         if(jumlah_CCTV >1){
+             return "Sesuai";
+         }
+         else if("Baik".equals(kondisi_CCTV)){
+             return "Sesuai";
+         }
+         else if("Buruk".equals(kondisi_CCTV)){
+             return "Tidak Sesuai";
+         }
+         else if("Kanan Depan".equals(posisi_CCTV)){
+             return "Sesuai";
+         }
+         else{
+             return "Inputan Salah";
+         }
+     }
+     abstract void analisis();
      }
 
