052fd57e9856dff947668e373662dd08d78e282e
SISKA001 <ayusiska.yuana001@gmail.com>
2015-05-10 16:20:31 +0700

    PBO

--- src/tb/analisis_kenyamanan_indoor.java
+++ src/tb/analisis_kenyamanan_indoor.java
@@ -13,37 +13,81 @@ import java.util.Scanner;
  *
  * @author Asus
  */
-public class analisis_kenyamanan_indoor {
-     //public static void main(String[] args){
+public abstract class analisis_kenyamanan_indoor {
+
+    //public static void main(String[] args){
+
     private String sirkuit_udara;
     private String pencahayaan;
     private String kelembapan;
     private int suhu_ruangan;
-    
-    Scanner scan = new Scanner (System.in);
-    
-    public void setsirkuit_udara (String sirkuit_udara){
+
+    Scanner scan = new Scanner(System.in);
+
+    public void setsirkuit_udara(String sirkuit_udara) {
         this.sirkuit_udara = sirkuit_udara;
-        }
-     public String getsirkuit_udara(){
-            return this.sirkuit_udara;
-        }
-      public void setpencahayaan (String pencahayaan){
+    }
+
+    public String getsirkuit_udara() {
+        return this.sirkuit_udara;
+    }
+
+    public void setpencahayaan(String pencahayaan) {
         this.pencahayaan = pencahayaan;
     }
-     public String getpencahayaan(){
-            return this.pencahayaan;
-        }
-      public void setkelembapan (String kelembapan){
-        this.kelembapan= kelembapan;
-         }
-     public String getkelembapan(){
-            return this.kelembapan;
-        }
-      public void setsuhu_ruangan (int suhu_ruangan){
+
+    public String getpencahayaan() {
+        return this.pencahayaan;
+    }
+
+    public void setkelembapan(String kelembapan) {
+        this.kelembapan = kelembapan;
+    }
+
+    public String getkelembapan() {
+        return this.kelembapan;
+    }
+
+    public void setsuhu_ruangan(int suhu_ruangan) {
         this.suhu_ruangan = suhu_ruangan;
+    }
+
+    public int getsuhu_ruangan() {
+        return this.suhu_ruangan;
+    }
+
+    String sirkuit_udara(String sirkuit_udara) {
+        if ("lancar".equals(sirkuit_udara)) {
+            return "Sesuai";
+        } else {
+            return "Tidak Sesuai";
         }
-     public int getsuhu_ruangan(){
-            return this.suhu_ruangan;
+    }
+
+    String pencahayaan(String pencahayaan) {
+        if ("Standar Kesehatan".equals(pencahayaan)) {
+            return "Sesuai";
+        } else {
+            return "Tidak Sesuai";
         }
+    }
+
+    String kelembapan(String kelembapan) {
+        if ("Sedang".equals(kelembapan)) {
+            return "Sesuai";
+        } else {
+            return "Tidak Sesuai";
+        }
+    }
+
+    String suhu(int suhu_ruangan) {
+        if (suhu_ruangan <= 25 && suhu_ruangan >= 30) {
+            return "Sesuai";
+        } else {
+            return "Tidak Sesuai";
+        }
+    }
+
+    abstract void analisis();
+
 }
