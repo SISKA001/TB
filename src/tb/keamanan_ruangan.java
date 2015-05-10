b5456d13ba6783ac6190938fa49b520d4c4d92a6
SISKA001 <ayusiska.yuana001@gmail.com>
2015-05-10 16:21:07 +0700

    PBO

--- src/tb/keamanan_ruangan.java
+++ src/tb/keamanan_ruangan.java
@@ -13,84 +13,66 @@ import java.util.Scanner;
  *
  * @author Asus
  */
-public class keamanan_ruangan {
+public abstract class keamanan_ruangan {
     private String kekokohan_ruangan;
-    private String keamana_kuncipintu;
     private String keamanan_kunci_cendela;
-    private String kemanan_LCD;
-    private String bahaya;
+    private String keamanan_LCD;
     private String keamanan_kuncipintu;
-    
-    Scanner scan = new Scanner (System.in);
     
       public void setkekokohan_ruangan (String kekokohan_ruangan){
         this.kekokohan_ruangan = kekokohan_ruangan;
-        String KHR =scan.nextLine();
-        if("Baik".equals(KHR))
-              System.out.println("Kondisi Sesuai");
-        else if("Buruk".equals(KHR))
-              System.out.println("Kondisi Tidak Sesuai");
-        else
-              System.out.println("Inputan Salah");
          }
      public String getkekokohan_ruangan(){
             return this.kekokohan_ruangan;
         }
-      public void setkeamanan_kuncipintu (String keamana_kuncipintu){
-        this.keamana_kuncipintu = keamanan_kuncipintu;
-        String KKP = scan.nextLine();
-        if("Baik".equals(KKP))
-              System.out.println("Kondisi Sesuai");
-        else if("Buruk".equals(KKP))
-              System.out.println("Kondisi Tidak Sesuai");
-        else
-              System.out.println("Inputan Salah");
-    }
+      public void setkeamanan_kuncipintu (String keamanan_kuncipintu){
+        this.keamanan_kuncipintu= keamanan_kuncipintu;
+       }
      public String getkeamanan_kuncipintu(){
-            return this.keamana_kuncipintu;
+            return this.keamanan_kuncipintu;
         }
       public void setkeamanan_kunci_cendela (String keamanan_kunci_cendela){
         this.keamanan_kunci_cendela = keamanan_kunci_cendela;
-        String KKC = scan.nextLine();
-        if("Baik".equals(KKC))
-              System.out.println("Kondisi Sesuai Kriteria");
-        else if("buruk".equals(KKC))
-              System.out.println("Kondisi Tidak Sesuai");
-        else
-              System.out.println("Inputan Salah");
-    }
+       }
      public String getkeamanan_kunci_cendela(){
             return this.keamanan_kunci_cendela;
         }
       public void setkeamanan_LCD (String keamanan_LCD){
-        this.kemanan_LCD = keamanan_LCD;
-        String KLCD = scan.nextLine();
-        if("Baik".equals(KLCD))
-              System.out.println("Kondisi Sesuai");
-        else if("Buruk".equals(KLCD))
-              System.out.println("Kondisi Tidak Sesuai");
-        else
-              System.out.println("Inputan Salah");
-    }
+        this.keamanan_LCD = keamanan_LCD;
+       }
      public String getkeamanan_LCD(){
-            return this.kemanan_LCD;
+            return this.keamanan_LCD;
         }
-      public void setbahaya (String bahaya){
-        this.bahaya = bahaya;
-        String BY = scan.nextLine();
-        if ("Terkendali".equals(BY))
-              System.out.println("Kondisi Sesuai");
-        else if("Berbahaya".equals(BY))
-              System.out.println("Kondisi Tidak Sesuai");
-        else
-              System.out.println("Inputan Salah");
-    }
-     public String getbahaya(){
-            return this.bahaya;
-        }
-
-    void setkeamanan_kunci_jendela(String nextLine) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+     String kekokohanRK(String kekokohan_ruangan){
+       if("Baik".equals(kekokohan_ruangan)){
+           return "Sesuai";
+       }
+       else if("Buruk".equals(kekokohan_ruangan)){
+           return "Tidak Sesuai";
+       }
+       else{
+           return "Inputan Salah";
+       }}
+   
+     String keamanan(String keamanan_LCD, String keamanan_kuncipintu, String kemanan_kunci_cendela){
+         if("Baik".equals(keamanan_LCD)){
+             return "Sesuai";
+         }
+         else if("Buruk".equals(keamanan_LCD)){
+             return "Tidak Sesuai";
+         }
+         else if("Baik".equals(keamanan_kuncipintu)){
+             return "Sesuai";
+         }
+         else if("Buruk".equals(keamanan_kuncipintu)){
+             return "Tidak Sesuai";
+         }
+         else if("Baik".equals(keamanan_kunci_cendela)){
+             return "Sesuai";
+         }
+         else{
+             return "inputan Salah"; 
+     }}
+     abstract void analisis();
 }
+
